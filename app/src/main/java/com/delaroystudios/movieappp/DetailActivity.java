0d8dbf4711dc565dcd260616217746e4135cc71b
Araiilym <170107085@stu.sdu.edu.kz>
2020-05-17 19:30:07 +0300

    codes

---
+++ app/src/main/java/com/delaroystudios/movieappp/DetailActivity.java
@@ -0,0 +1,104 @@
+package com.delaroystudios.movieappp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.delaroystudios.movieappp.model.Movie;
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+public class DetailActivity extends AppCompatActivity {
+    TextView nameOfMovie, plotSynopsis, userRating, releaseDate;
+    ImageView imageView;
+    private RecyclerView recyclerView;
+    private Movie favorite;
+    private final AppCompatActivity activity = DetailActivity.this;
+
+    Movie movie;
+    String thumbnail, movieName, synopsis, rating, dateOfRelease;
+    int movie_id;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        initCollapsingToolbar();
+
+        imageView = (ImageView) findViewById(R.id.thumbnail_image_header);
+        nameOfMovie = (TextView) findViewById(R.id.title);
+        plotSynopsis = (TextView) findViewById(R.id.plotsynopsis);
+        userRating = (TextView) findViewById(R.id.userrating);
+        releaseDate = (TextView) findViewById(R.id.releasedata);
+
+        Intent intentThatStartedThisActivity = getIntent();
+        if (intentThatStartedThisActivity.hasExtra("movies")) {
+
+            movie = getIntent().getParcelableExtra("movies");
+
+            thumbnail = movie.getPosterPath();
+            movieName = movie.getOriginalTitle();
+            synopsis = movie.getOverview();
+            rating = Double.toString(movie.getVoteAverage());
+            dateOfRelease = movie.getReleaseDate();
+            movie_id = movie.getId();
+
+            String poster = "https://image.tmdb.org/t/p/w500" + thumbnail;
+
+            Glide.with(this)
+                    .load(poster)
+                    .placeholder(R.drawable.load)
+                    .into(imageView);
+
+            nameOfMovie.setText(movieName);
+            plotSynopsis.setText(synopsis);
+            userRating.setText(rating);
+            releaseDate.setText(dateOfRelease);
+
+        } else {
+            Toast.makeText(this, "No API Data", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void initCollapsingToolbar(){
+        final CollapsingToolbarLayout collapsingToolbarLayout =
+                (CollapsingToolbarLayout) findViewById(R.id.collapsing_toolbar);
+        collapsingToolbarLayout.setTitle(" ");
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.appbar);
+        appBarLayout.setExpanded(true);
+
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener(){
+            boolean isShow = false;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset){
+                if (scrollRange == -1){
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0){
+                    collapsingToolbarLayout.setTitle(getString(R.string.movie_details));
+                    isShow = true;
+                }else if (isShow){
+                    collapsingToolbarLayout.setTitle(" ");
+                    isShow = false;
+                }
+            }
+        });
+    }
+
+    }
+
+
+
---
+++ app/src/main/java/com/delaroystudios/movieappp/SettingsActivity.java
@@ -0,0 +1,4 @@
+package com.delaroystudios.movieappp;
+
+public class SettingsActivity {
+}
